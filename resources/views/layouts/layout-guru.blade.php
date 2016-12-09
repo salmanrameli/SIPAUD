93c1e1a49fd79dfbde884cf01b17d3261a03a7d2
Salman Rameli <salmanrameli@hotmail.com>
2016-12-09 21:09:22 +0700

    menambah menu ubah nilai siswa

--- resources/views/layouts/layout-guru.blade.php
+++ resources/views/layouts/layout-guru.blade.php
@@ -80,6 +80,9 @@
                                 <a href="{{ route('guru.registrasi nilai') }}"><i class="fa fa-save fa-fw"></i> Registrasi Nilai Siswa</a>
                             </li>
                             <li>
+                                <a href="{{ route('guru.registrasi nilai') }}"><i class="fa fa-save fa-fw"></i> Ubah Nilai Siswa</a>
+                            </li>
+                            <li>
                                 <a href="{{ route('guru.nilai.all') }}"><i class="fa fa-book fa-fw"></i> Lihat Nilai Siswa</a>
                             </li>
                         </ul>
