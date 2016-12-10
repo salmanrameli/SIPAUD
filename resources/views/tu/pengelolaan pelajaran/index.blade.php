54bd064c18eb60369eed8c6f9efb908c48ce5e43
Salman Rameli <salmanrameli@hotmail.com>
2016-12-10 19:45:27 +0700

    menampilkan daftar semua pelajaran pada halaman pengelolaan pelajaran

--- resources/views/tu/pengelolaan pelajaran/index.blade.php
+++ resources/views/tu/pengelolaan pelajaran/index.blade.php
@@ -9,7 +9,20 @@
         <div class="col-lg-3">
             <a href="{{ route('tu.pelajaran.register') }}"><button type="button" class="btn btn-default btn-block">Registrasi Pelajaran</button></a>
             <br>
-            <a href="{{ route('tu.semua_pelajaran') }}"><button type="button" class="btn btn-default btn-block">Semua Pelajaran</button></a>
+            {{--<a href="{{ route('tu.semua_pelajaran') }}"><button type="button" class="btn btn-default btn-block">Semua Pelajaran</button></a>--}}
         </div>
+        <br><br>
+        <h3>Daftar Pelajaran</h3>
+        <table class="table">
+            @foreach($hasil as $pelajaran)
+                <tr>
+                    <td>{{ $pelajaran->nama }}</td>
+                    <td><div class="pull-right">
+                            <a href="{{ route('tu.pelajaran.show', ['id' => $pelajaran->id]) }}"><button type="button" class="btn btn-default">Lihat Pelajaran</button></a>
+                            <a href="{{ route('tu.pelajaran.edit', ['id' => $pelajaran->id]) }}"><button type="button" class="btn btn-info">Ubah Data</button></a>
+                        </div></td>
+                </tr>
+            @endforeach
+        </table>
     </div>
 @endsection
