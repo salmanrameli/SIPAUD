aba65838755f30a4b038a0e55f3ceb4932c6db3d
also known as Anugra Pratama Rameli <salmanrameli@hotmail.com>
2016-12-10 19:45:01 +0700

    menampilkan daftar semua petugas tata usaha pada halaman pengelolaan data tata usaha

--- resources/views/tu/pengelolaan tu/index.blade.php
+++ resources/views/tu/pengelolaan tu/index.blade.php
@@ -9,7 +9,20 @@
         <div class="col-lg-3">
             <a href="{{ route('tu.create') }}"><button type="button" class="btn btn-default btn-block">Registrasi Petugas TU</button></a>
             <br>
-            <a href="{{ route('tu.semua_tu') }}"><button type="button" class="btn btn-default btn-block">Lihat Petugas TU</button></a>
+            {{--<a href="{{ route('tu.semua_tu') }}"><button type="button" class="btn btn-default btn-block">Lihat Petugas TU</button></a>--}}
         </div>
+        <br><br>
+        <h3>Daftar Petugas Tata Usaha</h3>
+        <table class="table">
+            @foreach($hasil as $tu)
+                <tr>
+                    <td>{{ $tu->nama }}</td>
+                    <td><div class="pull-right">
+                            <a href="{{ route('tu.show', ['id' => $tu->id]) }}"><button type="button" class="btn btn-default">Lihat Petugas</button></a>
+                            <a href="{{ route('tu.edit', ['id' => $tu->id]) }}"><button type="button" class="btn btn-info">Ubah Data</button></a>
+                        </div></td>
+                </tr>
+            @endforeach
+        </table>
     </div>
     @endsection
