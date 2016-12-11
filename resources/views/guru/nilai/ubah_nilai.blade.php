1212774ddee121885710c04aaa0e395d0e6e1e4b
Salman Rameli <salmanrameli@hotmail.com>
2016-12-11 20:30:02 +0700

    menambah rute untuk mengubah data siswa, menyesuaikan label pada textbox, menambah tombol ubah nilai

--- resources/views/guru/nilai/ubah_nilai.blade.php
+++ resources/views/guru/nilai/ubah_nilai.blade.php
@@ -5,6 +5,8 @@
 @endsection
 
 @section('content')
+    {{ Form::model($nilai, ['method' => 'PATCH', 'route' => ['guru.nilai.update', $nilai->id, $nilai->nomor_induk]]) }}
+
     <div class="col-lg-12">
         <div class="form-group">
             {{ Form::label('id', 'ID Pelajaran', ['class' => 'control-label']) }}
@@ -12,18 +14,26 @@
         </div>
 
         <div class="form-group">
-            {{ Form::label('id', 'ID Pelajaran', ['class' => 'control-label']) }}
-            {{ Form::text('id', $nilai->id_guru, ['class' => 'form-control']) }}
+            {{ Form::label('id_guru', 'ID Guru Pengajar', ['class' => 'control-label']) }}
+            {{ Form::text('id_guru', $nilai->id_guru, ['class' => 'form-control']) }}
         </div>
 
         <div class="form-group">
-            {{ Form::label('id', 'ID Pelajaran', ['class' => 'control-label']) }}
-            {{ Form::text('id', $nilai->nomor_induk, ['class' => 'form-control']) }}
+            {{ Form::label('nomor_induk', 'Nomor Induk Siswa', ['class' => 'control-label']) }}
+            {{ Form::text('nomor_induk', $nilai->nomor_induk, ['class' => 'form-control']) }}
         </div>
 
         <div class="form-group">
-            {{ Form::label('id', 'ID Pelajaran', ['class' => 'control-label']) }}
-            {{ Form::text('id', $nilai->nilai, ['class' => 'form-control']) }}
+            {{ Form::label('nilai', 'Nilai Siswa', ['class' => 'control-label']) }}
+            {{ Form::text('nilai', $nilai->nilai, ['class' => 'form-control']) }}
         </div>
+
+        <div class="form-group">
+            {{ Form::label('komentar', 'Komentar Guru', ['class' => 'control-label']) }}
+            {{ Form::text('komentar', $nilai->komentar, ['class' => 'form-control']) }}
+        </div>
+
+        {{ Form::submit('Ubah Nilai', ['class' => 'btn btn-success']) }}
+        {{ Form::close() }}
     </div>
 @endsection
