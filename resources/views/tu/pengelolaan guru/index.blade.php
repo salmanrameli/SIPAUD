d62172d0a86e35cbe3e31a07337ebf61a401f06a
Salman Rameli <salmanrameli@hotmail.com>
2016-12-11 18:39:31 +0700

    menampilkan data semua guru pada halaman pengelolaan data guru

--- resources/views/tu/pengelolaan guru/index.blade.php
+++ resources/views/tu/pengelolaan guru/index.blade.php
@@ -9,7 +9,20 @@
         <div class="col-lg-3">
             <a href="{{ route('tu.registrasi_guru') }}"><button type="button" class="btn btn-default btn-block">Registrasi Guru</button></a>
             <br>
-            <a href="{{ route('tu.semua_guru') }}"><button type="button" class="btn btn-default btn-block">Lihat Guru</button></a>
+            {{--<a href="{{ route('tu.semua_guru') }}"><button type="button" class="btn btn-default btn-block">Lihat Guru</button></a>--}}
         </div>
+        <br><br>
+        <h3>Daftar Guru</h3>
+        <table class="table">
+            @foreach($hasil as $guru)
+                <tr>
+                    <td>{{ $guru->nama }}</td>
+                    <td><div class="pull-right">
+                            <a href="{{ route('tu.guru.show', ['id' => $guru->id]) }}"><button type="button" class="btn btn-default">Lihat Guru</button></a>
+                            <a href="{{ route('guru.edit', ['id' => $guru->id]) }}"><button type="button" class="btn btn-info">Ubah Data</button></a>
+                        </div></td>
+                </tr>
+            @endforeach
+        </table>
     </div>
 @endsection
